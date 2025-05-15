fc97df5fbb01093cff2e743ecf6edaee2a7d42df
AP Singh <aagampreet99@gmail.com>
2025-05-16 02:39:46 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(to right, #4facfe, #00f2fe)',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
